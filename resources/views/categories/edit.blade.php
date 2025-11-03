d1023f9fed62ca5ec058bf98353f1c002a3534df
Hikmah Alusmawati <als.hikmahh@gmail.com>
2025-11-03 11:25:15 +0800

    feat: tambah edit dan delete post

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
   <div class="md:flex md:items-center md:justify-between mb-6">
-    <h1 class="text-2xl font-semibold">Edit Category</h1>
+    <h1 class="text-2xl font-semibold">Edit Categories</h1>
   </div>
 
   <form action="{{ route('categories.update', $category) }}" method="POST" class="bg-white p-6 rounded">
